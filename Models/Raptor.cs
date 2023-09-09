c8ce1958c640bc13194bb6a3e9c5e1bf91a6707e
Travis Mix <travismix1980@gmail.com>
2023-09-09 10:35:49 -0700

    Added Validations to our model and some constraints to the model

--- Models/Raptor.cs
+++ Models/Raptor.cs
@@ -7,19 +7,33 @@ namespace RaptorsCoreRazor.Models
         // Id is required by database for primary key
         public int Id { get; set; }
 
+        [Range(1, 100)]
+        [Required]
         [Display (Name = "Player Number")]
         public int PlayerNumber { get; set; }
 
+        [Required]
+        [StringLength (60, MinimumLength = 3)]
         [Display (Name = "Player Name")]
         public string? Name { get; set; }
 
+        [Required]
+        [StringLength (60, MinimumLength = 3)]
         [Display (Name = "Player Position")]
         public string? PlayerPosition { get; set; }
 
+        [Required]
         [Display (Name = "Player Height")]
         public string? PlayerHeight { get; set; }
 
+        [Range (1, 100000000)]
+        [Required]
+        [DataType(DataType.Currency)]
         [Display (Name = "Player Salary")]
         public double PlayerSalary  { get; set; }
+
+        [StringLength (60, MinimumLength = 3)]
+        [Display (Name = "College")]
+        public string? PlayerCollege { get; set; }
     }
 }
